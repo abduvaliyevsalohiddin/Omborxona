d3cd6ef2e52a8c2cd8acf949801a7f356f2c2a5e
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-18 12:26:17 -0800

    17.11.2023 dasrdagi mashg'ulotlar

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "home.html")
+
+    def post(self, request):
+        pass
--- asosiy/models.py
+++ asosiy/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import Ombor
+
+
+class Mahsulot(models.Model):
+    nom = models.CharField(max_length=50)
+    brend = models.CharField(max_length=50)
+    olchov = models.CharField(max_length=50)
+    narx = models.PositiveIntegerField()
+    miqdor = models.PositiveSmallIntegerField()
+    kelgan_sana = models.DateField(null=True, blank=True)
+    ombor = models.ForeignKey(Ombor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nom}, {self.brend}"
+
+
+class Mijoz(models.Model):
+    ism = models.CharField(max_length=30)
+    nom = models.CharField(max_length=50)
+    manzil = models.CharField(max_length=50)
+    tel = models.CharField(max_length=15)
+    ombor = models.ForeignKey(Ombor, on_delete=models.CASCADE)
+    qarz = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.ism}, {self.nom}"
