0811bbf32012c496bfef1bbc7900a475ff9e0dbf
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-21 07:48:53 -0800

    20.11.2023 dasrdagi ishlar

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .models import *
+from datetime import date
 
 
 class BolimlarView(View):
@@ -12,9 +14,28 @@ class BolimlarView(View):
 
 class MahsulotlarView(View):
     def get(self, request):
-        return render(request, "products.html")
+        content = {
+            "mahsulotlar": Mahsulot.objects.filter(ombor=request.user),
+            "nom": request.user.nom.capitalize()
+        }
+        return render(request, "products.html", content)
+
+    def post(self, request):
+        Mahsulot.objects.create(
+            nom=request.POST.get("nom"),
+            brend=request.POST.get("brend"),
+            olchov=request.POST.get("olchov"),
+            narx=request.POST.get("narx"),
+            miqdor=request.POST.get("miqdor"),
+            kelgan_sana=date.today(),
+            ombor=request.user
+        )
+        return redirect("/asosiy/mahsulotlar/")
 
 
 class MijozlarView(View):
     def get(self, request):
-        return render(request, "clients.html")
+        content = {
+            "mijozlar": Mijoz.objects.filter(ombor=request.user),
+        }
+        return render(request, "clients.html", content)
