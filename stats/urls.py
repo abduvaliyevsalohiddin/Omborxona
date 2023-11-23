cf4436abfce528d9e71f225ea8f14fa87bea999a
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-23 12:24:57 -0800

    Statsni va stats updateni to'g'rilash

--- stats/urls.py
+++ stats/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("statislar/", StatisView.as_view()),
+    path("stats_update/<int:son>/", StatsUpdateView.as_view()),
 
 ]
--- stats/views.py
+++ stats/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from .models import *
 from accounts.models import *
 from asosiy.models import *
+from datetime import date
 
 
 class StatisView(View):
@@ -16,3 +17,42 @@ class StatisView(View):
             }
             return render(request, "stats.html", content)
         return redirect("/")
+
+    def post(self, request):
+        Statistika.objects.create(
+            mahsulot=Mahsulot.objects.get(id=request.POST.get("mahsulot")),
+            mijoz=Mijoz.objects.get(id=request.POST.get("mijoz")),
+            miqdor=request.POST.get("miqdor"),
+            summa=request.POST.get("summa"),
+            tolangan_summa=request.POST.get("tolangan_summa"),
+            sana=date.today(),
+            nasiya=request.POST.get("nasiya"),
+            ombor=request.user
+        )
+        return redirect("/stats/statislar/")
+
+
+class StatsUpdateView(View):
+    def get(self, request, son):
+        if request.user.is_authenticated:
+            stats = Statistika.objects.get(id=son)
+            if stats.ombor == request.user:
+                content = {
+                    "mahsulotlar": Mahsulot.objects.filter(ombor=request.user),
+                    "mijozlar": Mijoz.objects.filter(ombor=request.user),
+                    "stats": Statistika.objects.get(id=son)
+                }
+                return render(request, "stats_update.html", content)
+            return redirect("/stats/statislar/")
+        return redirect("/")
+
+    def post(self, request, son):
+        Statistika.objects.filter(id=son).update(
+            miqdor=request.POST.get("miqdor"),
+            summa=request.POST.get("summa"),
+            tolangan_summa=request.POST.get("tolangan_summa"),
+            nasiya=request.POST.get("nasiya"),
+            sana=date.today(),
+            ombor=request.user
+        )
+        return redirect("/stats/statislar/")
