60b56258e76eaa41d289f5fc111dd65ff9946557
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-27 01:43:33 -0800

    OMBORXONA YAKUN

--- stats/urls.py
+++ stats/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path("statislar/", StatisView.as_view()),
     path("stats_update/<int:son>/", StatsUpdateView.as_view()),
+    path("stats_delete/<int:son>/", DeleteStatis.as_view()),
 
 ]
--- stats/views.py
+++ stats/views.py
@@ -9,8 +9,15 @@ from datetime import date
 class StatisView(View):
     def get(self, request):
         if request.user.is_authenticated:
+            natija = Statistika.objects.filter(ombor=request.user)
+            soz = request.GET.get("soz")
+            if soz:
+                natija = natija.filter(mahsulot__nom__contains=soz
+                                       ) | natija.filter(mahsulot__brend__contains=soz
+                                       ) | natija.filter(mijoz__ism__contains=soz
+                                       ) | natija.filter(mijoz__nom__contains=soz)
             content = {
-                "statistikalar": Statistika.objects.filter(ombor=request.user),
+                "statistikalar": natija,
                 "mahsulotlar": Mahsulot.objects.filter(ombor=request.user),
                 "mijozlar": Mijoz.objects.filter(ombor=request.user),
                 "nom": request.user.nom.capitalize()
@@ -19,9 +26,12 @@ class StatisView(View):
         return redirect("/")
 
     def post(self, request):
+        mahsulot = Mahsulot.objects.get(id=request.POST.get("mahsulot"))
+        mijoz = Mijoz.objects.get(id=request.POST.get("mijoz"))
+
         Statistika.objects.create(
-            mahsulot=Mahsulot.objects.get(id=request.POST.get("mahsulot")),
-            mijoz=Mijoz.objects.get(id=request.POST.get("mijoz")),
+            mahsulot=mahsulot,
+            mijoz=mijoz,
             miqdor=request.POST.get("miqdor"),
             summa=request.POST.get("summa"),
             tolangan_summa=request.POST.get("tolangan_summa"),
@@ -29,7 +39,18 @@ class StatisView(View):
             nasiya=request.POST.get("nasiya"),
             ombor=request.user
         )
+        mahsulot.miqdor -= int(request.POST.get("miqdor"))
+        mahsulot.save()
+        mijoz.qarz += int(request.POST.get("nasiya"))
+        mijoz.save()
         return redirect("/stats/statislar/")
+
+
+class DeleteStatis(View):
+    def get(self, request, son):
+        if request.user.is_authenticated:
+            Statistika.objects.get(id=son).delete()
+            return redirect('/stats/statislar/')
 
 
 class StatsUpdateView(View):
@@ -47,6 +68,9 @@ class StatsUpdateView(View):
         return redirect("/")
 
     def post(self, request, son):
+        mahsulot = Mahsulot.objects.get(id=request.POST.get("mahsulot"))
+        mijoz = Mijoz.objects.get(id=request.POST.get("mijoz"))
+
         Statistika.objects.filter(id=son).update(
             miqdor=request.POST.get("miqdor"),
             summa=request.POST.get("summa"),
@@ -55,4 +79,9 @@ class StatsUpdateView(View):
             sana=date.today(),
             ombor=request.user
         )
+
+        mahsulot.miqdor -= int(request.POST.get("miqdor"))
+        mahsulot.save()
+        mijoz.qarz += int(request.POST.get("nasiya"))
+        mijoz.save()
         return redirect("/stats/statislar/")
